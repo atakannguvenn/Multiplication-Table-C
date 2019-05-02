e8e0ab13ab4f4285d46668c14c0a41d11fe823aa
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-05-02 10:34:59 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 int main()
 {
- int row,column,y,i;
-   row = 1;
-   printf("==========================================================");
-   printf("\n\t   Multiplication Table from 1 - 10");
-   printf("\n----------------------------------------------------------");
-   printf("\n      ");
-   for(i=1; i<=10; i++)
-      printf("%4d",i);
-   printf("\n----------------------------------------------------------\n");
-   do
-   {
-    printf("%4d |",row);
-      for(column=1; column<=10; column++)
-      {
-       y=row*column;
-         printf("%4d",y);
-      }
-      while(column<=10);
-      printf("\n");
-      row++;
-   }
-   while(row<=10);
-   printf("----------------------------------------------------------");
-   return 0;
+    int row,column,y,i;
+    row = 1;
+    printf("==========================================================");
+    printf("\n\t   Multiplication Table from 1 - 10");
+    printf("\n----------------------------------------------------------");
+    printf("\n      ");
+    for(i=1; i<=10; i++)
+        printf("%4d",i);
+    printf("\n----------------------------------------------------------\n");
+    do
+    {
+        printf("%4d |",row);
+        for(column=1; column<=10; column++)
+        {
+            y=row*column;
+            printf("%4d",y);
+        }
+        printf("\n");
+        row++;
+    }
+    while(row<=10);
+    printf("----------------------------------------------------------");
+    return 0;
 }
